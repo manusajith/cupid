b8925d15602557b577f933e966ffb896515196b1
Manu S Ajith (Neo) <neo@codingarena.in>
2012-08-13 16:24:21 +0530

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 =begin
-package   :   Cupid's Buzz
+package   :  	Cupid's Buzz
 authors   :     Manu S Ajith | neo@codingarena.in
-      :   Jijo Sunny   | jijo@rubykitchen.org
-copyright :   Copyright (C) 2012. All rights reserved.
+      	  :	Jijo Sunny   | jijo@rubykitchen.org
+copyright :	Copyright (C) 2012. All rights reserved.
 license   :     GNU General Public License version 2 or later; see LICENSE.txt
 website   :     http://codingarena.in
 repo      :     http://github.com/manusajith/cupid
